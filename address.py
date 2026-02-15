55050c915e3dce98600086306701c7f8187feb54
FriendAssKicker <118471720+FriendAssKicker@users.noreply.github.com>
2026-02-15 18:44:09 +0530

    linking

--- address.py
+++ address.py
@@ -1,21 +1,13 @@
-book = {}
+import time as t
+def cure():
+    for _ in range(1, 101):
+        # f = open("el-karte.txt", "a")
+        # t.sleep(0.05)
+        # f.writelines(f"{_} i love myself!\n")
+        # f.close()
+        pass
+    print("Before Name : ", __name__)
+    
 
-book['Tom'] = {
-    "name" : "Tom",
-    "address" : "Uppal",
-    "phno" : 9397939110
-}
-
-book['Jerry'] = {
-    "name" : "Jerry",
-    "address" : "NGRI",
-    "phno" : 9397339110
-}
-
-import json
-
-s = json.dumps(book)
-with open("el-krate.json", "w") as f:
-    f.write(s)
-
-print(s)
+if __name__ == "__main__":
+    cure()
---
+++ linker_main.py
@@ -0,0 +1,3 @@
+import address as ats
+ats.cure()
+
